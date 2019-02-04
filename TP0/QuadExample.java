b98d6221e64947e35eae39a51e7fe5afd3bb3d6a
Alexis Heloir <alexis.heloir@uphf.fr>
2019-02-04 16:22:11 +0100

    Resources nécessaires au secont cours. Classe OpenGLEnVrac et classe TextureLoader associée

--- TP0/QuadExample.java
+++ TP0/QuadExample.java
@@ -55,11 +55,11 @@ public class QuadExample {
             GL11.glColor3f(0.5f,0.5f,1.0f);
              
             // draw quad
-            GL11.glBegin(GL11.GL_QUADS);
-            GL11.glVertex2f(100,100);
-            GL11.glVertex2f(100+200,100);
-            GL11.glVertex2f(100+200,100+200);
-            GL11.glVertex2f(100,100+200);
+            GL11.glBegin(GL11.GL_LINE_LOOP);
+            GL11.glVertex2i(100,100);
+            GL11.glVertex2i(100+200,100);
+            GL11.glVertex2i(100+200,100+200);
+            GL11.glVertex2i(100,100+200);
             GL11.glEnd();
   
             Display.update();
